4c718c5cf0c6287564ee3f12575be3d0987b1c5a
Andrew Lahiff <alahiff@lcgui03.gridpp.rl.ac.uk>
2016-11-24 06:19:37 +0000

    Add defaults for adjustable parameters

--- image/initialization.sh
+++ image/initialization.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Defaults
+if [ -z "$SQUID_CACHE_SIZE" ]; then
+    export SQUID_CACHE_SIZE=70000
+fi
+
+if [ -z "$SQUID_CACHE_MEM" ]; then
+    export SQUID_CACHE_MEM=2048
+fi
+
+# Update configuration as necessary
 sed -i -- "s@SQUID_CACHE_SIZE@$SQUID_CACHE_SIZE@g" /etc/squid/squid.conf
 sed -i -- "s@SQUID_CACHE_MEM@$SQUID_CACHE_MEM@g" /etc/squid/squid.conf
 
@@ -14,5 +24,5 @@ chown -R squid:squid /var/cache/squid
 # Prepare the cache
 /usr/sbin/squid -z
 
-# Give the squid cache some time to rebuild
-sleep 5
+# Wait a moment
+sleep 2
