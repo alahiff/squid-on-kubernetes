941ef00b87018118514cad71801b48052416228a
Andrew Lahiff <alahiff@lcgui03.gridpp.rl.ac.uk>
2017-02-07 13:01:37 +0000

    Allow log dir & cache dir to be specified by env variables

--- image/initialization.sh
+++ image/initialization.sh
@@ -9,17 +9,27 @@ if [ -z "$SQUID_CACHE_MEM" ]; then
     export SQUID_CACHE_MEM=2048
 fi
 
+if [ -z "$SQUID_LOG_DIR" ]; then
+    export SQUID_LOG_DIR=/var/log/squid
+fi
+
+if [ -z "$SQUID_CACHE_DIR" ]; then
+    export SQUID_CACHE_DIR=/var/cache/squid
+fi
+
 # Update configuration as necessary
 sed -i -- "s@SQUID_CACHE_SIZE@$SQUID_CACHE_SIZE@g" /etc/squid/squid.conf
 sed -i -- "s@SQUID_CACHE_MEM@$SQUID_CACHE_MEM@g" /etc/squid/squid.conf
+sed -i -- "s@SQUID_LOG_DIR@$SQUID_LOG_DIR@g" /etc/squid/squid.conf
+sed -i -- "s@SQUID_CACHE_DIR@$SQUID_CACHE_DIR@g" /etc/squid/squid.conf
 
 # Log dir
-mkdir -p /var/log/squid
-chown -R squid:squid /var/log/squid
+mkdir -p $SQUID_LOG_DIR
+chown -R squid:squid $SQUID_LOG_DIR
 
 # Cache dir
-mkdir -p /var/cache/squid
-chown -R squid:squid /var/cache/squid
+mkdir -p $SQUID_CACHE_DIR
+chown -R squid:squid $SQUID_CACHE_DIR
 
 # Prepare the cache
 /usr/sbin/squid -z
